923af890570914a6ef9068d79790aecfe44b0b22
thalisson1980 <76484672+thalisson1980@users.noreply.github.com>
2022-06-19 16:13:15 +0100

    concluido

--- src/main/scala/org/xpandit/App.scala
+++ src/main/scala/org/xpandit/App.scala
@@ -1,10 +1,11 @@
 package org.xpandit
 
 
-import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql.{Row, SaveMode, SparkSession}
 import org.apache.spark.sql.functions.{col, desc}
-import org.apache.spark.sql.types.{DoubleType, FloatType, IntegerType, StringType, StructField, StructType}
+import org.apache.spark.sql.types.{ArrayType, DoubleType, FloatType, IntegerType, StringType, StructField, StructType}
 import org.json4s.scalap.scalasig.ClassFileParser.header
+
 import scala.collection.mutable.ArrayBuffer
 import org.apache.spark.sql.functions.regexp_replace
 
@@ -19,7 +20,6 @@ object App {
     val spark = SparkSession.builder().config("spark.sql.warehouse.dir", "C:/Users/Thalisson/Desktop/xpandit2/desafio/src/main/resources/csvfiles/").appName("Desafio").master("local[2]").getOrCreate()
 
 
-
     val simpleSchema = StructType(Array(
       StructField("App",StringType,true),
       StructField("Category",StringType,true),
@@ -29,44 +29,45 @@ object App {
       StructField("Installs", StringType, true),
       StructField("Type", StringType, true),
       StructField("Price", DoubleType, true),
-      StructField("Content Rating", StringType, true),
+      StructField("ContentRating", StringType, true),
       StructField("Genres", StringType, true),
-      StructField("Last Updated", StringType, true),
-      StructField("Current Ver", StringType, true),
-      StructField("Android Ver", StringType, true)
+      StructField("LastUpdated", StringType, true),
+      StructField("CurrentVer", StringType, true),
+      StructField("AndroidVer", StringType, true)
 
     ))
+    
 
     val df2 = spark.read.option("header",true).schema(simpleSchema).options(Map("inferSchema"->"true","delimiter"->","))
-      .csv("C:/Users/Thalisson/Desktop/xpandit2/desafio/src/main/resources/csvfiles/googleplaystore.csv").orderBy(desc("Rating"))
+      .csv("C:/Users/Thalisson/Desktop/xpandit2/desafio/src/main/resources/csvfiles/googleplaystore.csv").orderBy(desc("Reviews"))
 
 
     val df3 = df2.withColumn("AppNew",regexp_replace(df2("App"),"'",""))
     df3.createOrReplaceGlobalTempView("apps")
-    val df4 = df3.groupBy("AppNew").max("Rating").collect()
+    val df4 = df3.groupBy("AppNew").max("Reviews").collect()
 
 
+        val apps: ArrayBuffer[String] = new ArrayBuffer[String]
+        df4.foreach(app =>{
+          apps.append(app.getString(0))
 
-    val apps: ArrayBuffer[String] = new ArrayBuffer[String]
-    val apps2: ArrayBuffer[Object] = new ArrayBuffer[Object]
-    df4.foreach(app =>{
-      apps.append(app.getString(0))
-
-    })
-    apps.foreach(app =>{
-     /*val auxdf =  df3.filter(s"AppNew == '$app'")
-     if( auxdf.count() > 1){
-       var greaterRating = 0.0
-       val categories: ArrayBuffer[String] = new ArrayBuffer[String]
-        auxdf.foreach( aux => {
-            if(greaterRating <= aux.getAs[Double](2)){
-
-            }
         })
-     }
-*/
-      apps2.append(spark.sql(s"SELECT first(App),(Select distinct(Category) from global_temp.apps where app == '$app' ) as Categories, max(Rating) as maxRating FROM global_temp.apps where AppNew == '$app' ").collect())
-    })
+        var newDf =  spark.sql(
+          s"""SELECT first(App), collect_set(Category) as Values,
+             | first(Rating), max(Reviews),first(Size),first(Installs),first(Type),first(Price),
+             | first(ContentRating),first(Genres),first(LastUpdated),first(CurrentVer),first(AndroidVer)
+             |  FROM global_temp.apps where AppNew == '${apps.last}' """.stripMargin).toDF()
+
+
+            apps.foreach(app =>{
+          var auxDf = spark.sql(
+                s"""SELECT first(App), collect_set(Category) as Values,
+                   | first(Rating), max(Reviews),first(Size),first(Installs),first(Type),first(Price),
+                   | first(ContentRating),first(Genres),first(LastUpdated),first(CurrentVer),first(AndroidVer)
+                   |  FROM global_temp.apps where AppNew == '$app' """.stripMargin).toDF()
+
+              newDf = newDf.union(auxDf)
+        })
 
   }
 
